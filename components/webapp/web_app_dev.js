c546ac1a61b21fcde97b9f03a6585a625785d7d5
JasYi <44511533+JasYi@users.noreply.github.com>
2024-07-08 23:51:28 -0700

    added image translation

--- components/webapp/web_app_dev.js
+++ components/webapp/web_app_dev.js
@@ -5,6 +5,13 @@ import { React, useEffect, useState } from "react";
 export default function WebAppDevPage() {
   const projects = [
     {
+      name: "Splitify",
+      tech: ["React", "Flask", "OAuth2.0", "Spotify API"],
+      description: "Splitting your large Spotify playlists into subplaylists",
+      link: "https://splitify-jasyi.vercel.app/",
+      img: "/splitify.png",
+    },
+    {
       name: "Filed.ai",
       tech: ["OCR", "GPT-4", "Flask", "NextJS"],
       description:
@@ -13,19 +20,12 @@ export default function WebAppDevPage() {
       img: "/filedai.png",
     },
     {
-      name: "Splitify",
-      tech: ["React", "Flask", "OAuth2.0", "Spotify API"],
-      description: "Splitting your large Spotify playlists into subplaylists",
-      link: "https://splitify-jasyi.vercel.app/",
-      img: "/splitify.png",
-    },
-    {
-      name: "Oyster",
-      tech: ["React", "FastAPI", "MongoDB"],
+      name: "Infrared Image Translation",
+      tech: ["GANs", "PyTorch"],
       description:
-        "Connecting small businesses to investors | Winner @ Georgetown Hackathon '24",
-      link: "https://devpost.com/software/project-yay-bpqg7y",
-      img: "/oystergif.gif",
+        "Translating visible light images to infrared images using GANs to detect pedestrians | Final Project for Graduate Level Class",
+      link: "https://github.com/JasYi/CycleGAN-llvip",
+      img: "/llvip.png",
     },
     {
       name: "XLR8",
@@ -34,6 +34,14 @@ export default function WebAppDevPage() {
         "Creating better developer tools for hardware engineers working with FPGAs | Stanford Hackathon '24",
       link: "https://devpost.com/software/xlr8",
       img: "/xlr8.jpg",
+    },
+    {
+      name: "Oyster",
+      tech: ["React", "FastAPI", "MongoDB"],
+      description:
+        "Connecting small businesses to investors | Winner @ Georgetown Hackathon '24",
+      link: "https://devpost.com/software/project-yay-bpqg7y",
+      img: "/oystergif.gif",
     },
   ];
   return (
